e0f01937559504d3451b5328326f13788843f4e0
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2023-08-27 15:29:39 -0400

    SBC

--- SpringBootCache/src/test/java/com/example/caching/SpringBootCache/SpringBootCacheApplicationTests.java
+++ SpringBootCache/src/test/java/com/example/caching/SpringBootCache/SpringBootCacheApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.caching.SpringBootCache;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SpringBootCacheApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.example.caching.SpringBootCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
