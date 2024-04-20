37a45dfaaa45a1f9937d8a1264f692bbfd7e74e4
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2024-04-20 10:27:15 -0400

    CB

--- ServiceA/src/main/java/com/example/serviceA/controller/ServiceAController.java
+++ ServiceA/src/main/java/com/example/serviceA/controller/ServiceAController.java
@@ -8,18 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.sql.DatabaseMetaData;
-import java.util.Date;
-
 @RestController
 @RequestMapping("/a")
 public class ServiceAController {
 
-    int attempt = 1;
     @GetMapping("/serviceA")
-    //@CircuitBreaker(name = "serviceA", fallbackMethod = "serviceAFallBack")
-    //@Retry(name = "serviceA")//, fallbackMethod = "serviceAFallBack")
-    @RateLimiter(name = "serviceA", fallbackMethod = "serviceAFallBack")
+    @CircuitBreaker(name = "serviceA", fallbackMethod = "serviceAFallBack")
     public String serviceA(){
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject("http://localhost:8091/b/serviceB", String.class);
