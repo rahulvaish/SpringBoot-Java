594c4a15dad0778fb1dc727f19db6679efb52ba6
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2024-05-01 07:16:34 -0400

    Arch Testing

---
+++ SpringBootArchTesting/src/test/java/com/example/SpringBootArchTesting/SpringBootArchTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootArchTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArchTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
